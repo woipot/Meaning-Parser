2877bf65ff401e2746e3457a3dff4a8ac33e929e
woipot <frostik0409@gmail.com>
2021-01-04 04:15:58 +0300

    add documentation to pikabu parser

--- pikabuparser.py
+++ pikabuparser.py
@@ -1,28 +1,23 @@
-from abc import ABC, abstractmethod
+import html
+import logging
+import re
+import time
 
-from webdriver_manager.chrome import ChromeDriverManager
-
+from bs4 import BeautifulSoup
 from pymongo import MongoClient, errors
 from selenium import webdriver
-from bs4 import BeautifulSoup
-from threading import Thread
-
-import hashlib
-import re
-import html5lib
-import html
-import requests
-import time
-import logging
-import json
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-class Pikabu_Parser():
+class PikabuParser:
     def __init__(self, urls):
         self.urls = urls
 
-    def get_webdriver(self, path_to_webdriver_binary: str, is_headless: bool = True):
+    def initWebdriver(self, is_headless: bool = True):
+        """
 
+        :param is_headless: open browser window or work silent
+        """
         options = webdriver.ChromeOptions()
 
         if is_headless:
@@ -31,20 +26,24 @@ class Pikabu_Parser():
         self.driver = webdriver.Chrome(
             ChromeDriverManager().install(), options=options)
 
-    def get_db_collection(self):
+    def initDataBase(self):
         client = MongoClient("mongodb://woipot:woipot@aqulasoft.com/daryana")
         self.clinent = client
         db = client.web_parser
         self.db_collection = db.pikabu
 
-    def start_parse(self):
+    def startParse(self):
+        """
+        !IMPORTANT to correct work you need setup breakpoint and log in to pikabu account and activate endless scroll
+        NOT PAGES
+        """
         for url in self.urls:
-            self.__get_content__(html.unescape(url))
+            self.parseTag(html.unescape(url))
 
         self.driver.quit()
         self.clinent.close()
 
-    def __get_content__(self, url):
+    def parseTag(self, url):
         logging.info(url)
         self.driver.get(url)
 
@@ -71,11 +70,11 @@ class Pikabu_Parser():
             logging.info(f'Iterations made: {i}')
 
             requiredHtml = self.driver.page_source
-            self.__parse_page__(requiredHtml)
+            self.parseArticle(requiredHtml)
 
         logging.info(f'Total iterations made: {i}')
 
-    def __parse_page__(self, requiredHtml):
+    def parseArticle(self, requiredHtml):
 
         soup = BeautifulSoup(requiredHtml, 'html5lib')
 
@@ -93,10 +92,34 @@ class Pikabu_Parser():
                              content=re.sub(r'[\t\v\r\n\f]+', ' ', content.text))
 
                 if len(story["content"]) > 300:
-                    self.__load_to_db__(story)
-    def __load_to_db__(self, story: dict) -> None:
+                    self.saveToDb(story)
+
+    def saveToDb(self, story: dict) -> None:
         try:
             self.db_collection.insert_one(story)
             logging.info(f'Stories in DB: {self.db_collection.count()}')
         except errors.DuplicateKeyError:
             return
+
+
+if __name__ == "__main__":
+
+    pikabu_urls = [
+        "https://pikabu.ru/tag/iphone",
+        "https://pikabu.ru/tag/apple",
+        "https://pikabu.ru/tag/ios",
+        "https://pikabu.ru/tag/macos",
+        "https://pikabu.ru/tag/macbook",
+        "https://pikabu.ru/tag/mac",
+        "https://pikabu.ru/tag/imac",
+        "https://pikabu.ru/tag/ipad",
+        "https://pikabu.ru/tag/Стив%20Джобс",
+        "https://pikabu.ru/tag/airpods",
+    ]
+
+    parser = PikabuParser(pikabu_urls)
+    parser.initWebdriver(is_headless=False)
+    parser.initDataBase()
+    parser.startParse()
+
+    logging.info('Program finished\n')
